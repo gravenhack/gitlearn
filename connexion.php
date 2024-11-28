28ec2a17827268824c4089a68ecf458c060bdac8
callofcode <ahodlendac@gmail.com>
2024-11-28 13:33:24 +0100

    création de la page de connexion

---
+++ connexion.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>connexion</title>
+</head>
+<body>
+    <form action="#">
+        <label for="">Name</label>
+        <input type="text">
+
+        <label for="">Firstname</label>
+        <input type="text">
+
+        <button type="submit"></button>
+    </form>
+<?php
+
+?>
+</body>
+</html>
